ec5af9663bdd6f3949c86cbe354634853cb8c951
Jin Young, Yoo <yoojinyoung@kakao.com>
2024-05-29 10:29:44 +0900

    chore: App에 Controller 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { useToast } from "./contexts/toast.context";
+import Controller from "./components/Controller";
 
 function App() {
-  const toast = useToast();
   return (
-    <main>
-      <h1>토스트</h1>
-
-      <button
-        onClick={() =>
-          toast({
-            title: "Scheduled: Catch up",
-            description: "Friday, February 10, 2023 at 5:57 PM",
-          })
-        }
-      >
-        토스트 띄우기
-      </button>
+    <main className="h-screen grid place-items-center">
+      <Controller />
     </main>
   );
 }
